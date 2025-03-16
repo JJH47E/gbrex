5568f1eff8571ef451007ddec6a8c29e84bc2063
JJH <62762383+JJH47E@users.noreply.github.com>
2025-03-16 13:34:58 +0000

    small refactor
    move game logic into own file

---
+++ game.c
@@ -0,0 +1,37 @@
+#include "trex_init.c"
+#include "cacti.c"
+#include "globals.c"
+#include "scoreboard.c"
+#include <stdbool.h>
+
+#ifndef GAME_C
+#define GAME_C
+
+bool isPaused = false;
+
+void init_game() {
+  init_trex();
+  init_cacti();
+  init_score();
+}
+
+void tick_game() {
+  if (!isPaused) {
+    tick_trex();
+    tick_cactus();
+    tick_score();
+
+    if (joypad() & J_A) {
+      try_jump_trex();
+    }
+  } else {
+
+  }
+
+  if (joypad() & J_START) {
+    isPaused = !isPaused;
+    waitpadup();
+  }
+}
+
+#endif
--- trex.c
+++ trex.c
@@ -2,13 +2,7 @@
 #include <gb/drawing.h>
 #include <stdio.h>
 #include <string.h>
-#include "trex_init.c"
-#include "cacti.c"
-#include "globals.c"
-#include "scoreboard.c"
-#include <stdbool.h>
-
-bool isPaused = false;
+#include "game.c"
 
 enum State {
   MENU,
@@ -23,9 +17,7 @@ void main(void)
   game_state = INGAME;
   set_increment_score_callback(increment_score);
 
-  init_trex();
-  init_cacti();
-  init_score();
+  init_game();
   
   SHOW_SPRITES;
   
@@ -33,22 +25,7 @@ void main(void)
     vsync();
 
     if (game_state == INGAME) {
-      if (!isPaused) {
-        tick_trex();
-        tick_cactus();
-        tick_score();
-
-        if (joypad() & J_A) {
-          try_jump_trex();
-        }
-      } else {
-
-      }
-
-      if (joypad() & J_START) {
-        isPaused = !isPaused;
-        waitpadup();
-      }
+      tick_game();
     }
 
     delay(15);
