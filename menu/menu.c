708d45fffc4b9373147e9275f9d732c03f845d8d
JJH <62762383+JJH47E@users.noreply.github.com>
2025-03-16 15:40:21 +0000

    finish title screen

--- menu/menu.c
+++ menu/menu.c
@@ -14,25 +14,25 @@ bool is_tip_shown = true;
 void init_menu() {
   for (uint8_t i = 0; i < 24; i++) {
     set_sprite_tile(i, 100 + i);
-    uint8_t x = 50 + (8 * (i%8));
-    uint8_t y = 50 + (8 * (i/8));
+    uint8_t x = 56 + (8 * (i%8));
+    uint8_t y = 30 + (8 * (i/8));
     move_sprite(i, x, y);
   }
 
   // Setup "Press Start"
-  write_text("Press", 5, 24, 50, 120);
-  write_text("Start", 5, 29, 98, 120);
+  write_text("Press", 5, 24, 44, 120);
+  write_text("Start", 5, 29, 92, 120);
 
   toggle_tip_ticks = TICK_TOGGLE;
 }
 
 void show_text() {
   for (uint8_t i = 0; i < 5; i++) {
-    move_sprite(24 + i, 50 + (i * 8), 120);
+    move_sprite(24 + i, 44 + (i * 8), 120);
   }
 
   for (uint8_t i = 0; i < 5; i++) {
-    move_sprite(29 + i, 98 + (i * 8), 120);
+    move_sprite(29 + i, 92 + (i * 8), 120);
   }
 }
 
